3b7fe31d7389dc56fa554f91ffa08558bf5834c2
James Byun <jbyun@atlassian.com>
2024-09-09 23:39:49 +1000

    fix for confluence request

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,32 +1,57 @@
-import Resolver from '@forge/resolver';
+import Resolver from "@forge/resolver";
 import api, { route, asApp, asUser } from "@forge/api";
 
 const resolver = new Resolver();
 
-resolver.define('getText', async (req) => {
+resolver.define("getText", async (req) => {
   console.log(req);
-  console.log('loggedin user', req.context.accountId);
+  console.log("loggedin user", req.context.accountId);
   const accountid = req.context.accountId;
-  const res = await asApp().requestJira(route `/rest/api/3/user/email?accountId=${accountid}`);
+  const installationContext = req.context.installContext;
+  const isJira = installationContext.includes("jira");
+  console.log("installationContext", installationContext);
+  let res;
+  if (isJira) {
+    res = await asApp().requestJira(
+      route`/rest/api/3/user/email?accountId=${accountid}`
+    );
+  } else {
+    res = await asApp().requestConfluence(
+      route`/wiki/rest/api/3/user/email?accountId=${accountid}`
+    );
+  }
+
   const data = await res.text();
-  console.log('res', data);
+  console.log("res", data);
   return data;
 });
 
-resolver.define('getText2', async (req) => {
+resolver.define("getText2", async (req) => {
   console.log(req);
-  console.log('loggedin user', req.context.accountId);
+  console.log("loggedin user", req.context.accountId);
   const accountid = req.context.accountId;
+  const installationContext = req.context.installContext;
+  const isJira = installationContext.includes("jira");
+  console.log("installationContext", installationContext);
   let data;
   try {
-    const res = await asUser().requestJira(route `/rest/api/3/user/email?accountId=${accountid}`);
+    let res;
+    if (isJira) {
+      res = await asUser().requestJira(
+        route`/rest/api/3/user/email?accountId=${accountid}`
+      );
+    } else {
+      res = await asUser().requestConfluence(
+        route`/wiki/rest/api/3/user/email?accountId=${accountid}`
+      );
+    }
     data = await res.text();
-    console.log('res-asuser', data);
+    console.log("res-asuser", data);
   } catch (ex) {
-    console.log('error-asuser', ex);
+    console.log("error-asuser", ex);
     data = ex.message;
   }
-  
+
   return data;
 });
 
