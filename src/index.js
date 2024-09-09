47bc11425133aa31450f7fec5cfa1dcd4b88ebe1
James Byun <jbyun@atlassian.com>
2024-09-10 01:33:19 +1000

    support confluence in webtrigger for email access

--- src/index.js
+++ src/index.js
@@ -124,35 +124,171 @@ exports.runWebTriggerForUser = async (request) => {
 
 exports.runWebTriggerForEmail = async (request) => {
   try {
+    console.log('request (webtrigger)', request);
     const reqBody = JSON.parse(request.body);
     const accountid_list = reqBody.accountid_list;
+    const accountIds_queryparams = accountid_list.map(a => `accountId=${a}`).join('&');
+    console.log('accountIds_queryparams', accountIds_queryparams);
+    const product = reqBody.product && reqBody.product === 'confluence' ? 'confluence' : 'jira';
+    console.log('product', product);
 
     let results = [];
-    for (var i = 0; i < accountid_list.length; i++) {
-      const accountid = accountid_list[i];
+    if (product === 'jira') {
+      for (var i = 0; i < accountid_list.length; i++) {
+        const accountid = accountid_list[i];
+        try {
+          const res = await asApp().requestJira(
+            route`/rest/api/3/user/email?accountId=${accountid}`
+          );
+  
+          const data = await res.text();
+          results.push({
+            message: `result of ${accountid} email`,
+            result: data,
+          });
+  
+          const res3 = await asUser().requestJira(
+            route`/rest/api/3/user/email?accountId=${accountid}`
+          );
+  
+          const data3 = await res3.text();
+          results.push({
+            message: `result of ${accountid} email (asUser)`,
+            result: data3,
+          });
+        } catch (err) {
+          results.push({
+            message: `result of ${accountid}`,
+            result: `(error) ${err.message}`,
+          });
+        }
+      }
+  
+      // consuming bulk section;
       try {
-        const res = await asApp().requestJira(
-          route`/rest/api/3/user/email?accountId=${accountid}`
+        console.log('>>>1', `/rest/api/3/user/email/bulk?${accountIds_queryparams}`);
+        const res2 = await asApp().requestJira(
+          route`/rest/api/3/user/email/bulk?${accountIds_queryparams.toString()}`
         );
-
-        const data = await res.text();
-        results.push({
-          message: `result of ${accountid} email`,
-          result: data,
-        });
-
-        const res2 = await asUser().requestJira(
-          route`/rest/api/3/user/email?accountId=${accountid}`
-        );
-
+    
         const data2 = await res2.text();
         results.push({
-          message: `result of ${accountid} email (asUser)`,
+          message: `result of ${accountid_list.toString()} email/bulk`,
           result: data2,
+        });
+  
+        // console.log(`/rest/api/3/user/email/bulk?${accountIds_queryparams}`);
+        console.log('>>>2', `/rest/api/3/user/email/bulk?accountId=${accountid_list[0]}&accountId=${accountid_list[1]}`);
+        const res100 = await asApp().requestJira(
+          route`/rest/api/3/user/email/bulk?accountId=${accountid_list[0]}&accountId=${accountid_list[1]}`
+        );
+    
+        const data100 = await res100.text();
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk`,
+          result: data100,
         });
       } catch (err) {
         results.push({
-          message: `result of ${accountid}`,
+          message: `result of ${accountid_list.toString()} email/bulk`,
+          result: `(error) ${err.message}`,
+        });
+      }
+      
+  
+      try {
+        const res4 = await asUser().requestJira(
+          route`/rest/api/3/user/email/bulk?${accountIds_queryparams}`
+        );
+    
+        const data4 = await res4.text();
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk (asUser)`,
+          result: data4,
+        });
+      } catch (err) {
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk (asUser)`,
+          result: `(error) ${err.message}`,
+        });
+      }
+    } else {
+      // Confluence request
+      for (var i = 0; i < accountid_list.length; i++) {
+        const accountid = accountid_list[i];
+        try {
+          const res = await asApp().requestConfluence(
+            route`/wiki/rest/api/3/user/email?accountId=${accountid}`
+          );
+  
+          const data = await res.text();
+          results.push({
+            message: `result of ${accountid} email`,
+            result: data,
+          });
+  
+          const res3 = await asUser().requestConfluence(
+            route`/wiki/rest/api/3/user/email?accountId=${accountid}`
+          );
+  
+          const data3 = await res3.text();
+          results.push({
+            message: `result of ${accountid} email (asUser)`,
+            result: data3,
+          });
+        } catch (err) {
+          results.push({
+            message: `result of ${accountid}`,
+            result: `(error) ${err.message}`,
+          });
+        }
+      }
+  
+      // consuming bulk section;
+      try {
+        console.log('>>>1', `/rest/api/3/user/email/bulk?${accountIds_queryparams}`);
+        const res2 = await asApp().requestConfluence(
+          route`/wiki/rest/api/3/user/email/bulk?${accountIds_queryparams}`
+        );
+    
+        const data2 = await res2.text();
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk`,
+          result: data2,
+        });
+  
+        // console.log(`/rest/api/3/user/email/bulk?${accountIds_queryparams}`);
+        console.log('>>>2', `/rest/api/3/user/email/bulk?accountId=${accountid_list[0]}&accountId=${accountid_list[1]}`);
+        const res100 = await asApp().requestConfluence(
+          route`/wiki/rest/api/3/user/email/bulk?accountId=${accountid_list[0]}&accountId=${accountid_list[1]}`
+        );
+    
+        const data100 = await res100.text();
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk`,
+          result: data100,
+        });
+      } catch (err) {
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk`,
+          result: `(error) ${err.message}`,
+        });
+      }
+      
+  
+      try {
+        const res4 = await asUser().requestConfluence(
+          route`/wiki/rest/api/3/user/email/bulk?${accountIds_queryparams}`
+        );
+    
+        const data4 = await res4.text();
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk (asUser)`,
+          result: data4,
+        });
+      } catch (err) {
+        results.push({
+          message: `result of ${accountid_list.toString()} email/bulk (asUser)`,
           result: `(error) ${err.message}`,
         });
       }
